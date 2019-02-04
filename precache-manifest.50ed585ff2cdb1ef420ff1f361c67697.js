fe94f32a57e0ca244ccdaa52f8ecb9a12c62eeec
Mohan Singh <mslogicmaster@gmail.com>
2019-02-04 20:08:32 +0530

    Updated base path

---
+++ precache-manifest.50ed585ff2cdb1ef420ff1f361c67697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7baace3c3e7575c8f1e0fca9aa9a9d",
+    "url": "/demo/static/media/jsoneditor-icons.8e7baace.svg"
+  },
+  {
+    "revision": "df776aaf92209c815866",
+    "url": "/demo/static/js/runtime~main.df776aaf.js"
+  },
+  {
+    "revision": "2a9d06fcc31260d8d6cb",
+    "url": "/demo/static/js/main.2a9d06fc.chunk.js"
+  },
+  {
+    "revision": "060c0b12318273bf4e42",
+    "url": "/demo/static/js/1.060c0b12.chunk.js"
+  },
+  {
+    "revision": "2a9d06fcc31260d8d6cb",
+    "url": "/demo/static/css/main.7b5f1676.chunk.css"
+  },
+  {
+    "revision": "060c0b12318273bf4e42",
+    "url": "/demo/static/css/1.213110bd.chunk.css"
+  },
+  {
+    "revision": "28c91f17e8f7d5a3d19b334a4d403ca4",
+    "url": "/demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e751bf2682fce1bab18e789f40e17f41.js"
+  "/demo/precache-manifest.50ed585ff2cdb1ef420ff1f361c67697.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
