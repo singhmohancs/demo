4974204f2c2a696c6a77dd678f3fed21b004ecd4
Mohan Singh <mslogicmaster@gmail.com>
2019-02-04 19:55:08 +0530

    Initial commit

---
+++ precache-manifest.e751bf2682fce1bab18e789f40e17f41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7baace3c3e7575c8f1e0fca9aa9a9d",
+    "url": "/static/media/jsoneditor-icons.8e7baace.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72d96e7b28890f673c96",
+    "url": "/static/js/main.72d96e7b.chunk.js"
+  },
+  {
+    "revision": "060c0b12318273bf4e42",
+    "url": "/static/js/1.060c0b12.chunk.js"
+  },
+  {
+    "revision": "72d96e7b28890f673c96",
+    "url": "/static/css/main.2e805111.chunk.css"
+  },
+  {
+    "revision": "060c0b12318273bf4e42",
+    "url": "/static/css/1.213110bd.chunk.css"
+  },
+  {
+    "revision": "7cf63d505af52f058b6750b390a2c2bc",
+    "url": "/index.html"
+  }
+];
